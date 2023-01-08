8e1b65b7d4460fd30ba78566118db752d24cf72c
mazicwong <mazicwong@gmail.com>
2023-01-08 14:51:27 +0800

    .

--- parse.sh
+++ parse.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+# python src_joint/main.py parse \
+# --dataset ptb \
+# --save-per-sentences 1000 \
+# --eval-batch-size 50 \
+# --input-path input_s.txt \
+# --output-path-synconst output_synconst.txt \
+# --output-path-syndep output_syndephead.txt \
+# --output-path-synlabel output_syndeplabel.txt \
+# --embedding-path data/glove.gz \
+# --model-path-base models/joint_bert_dev=95.55_devuas=96.67_devlas=94.86.pt
+
+
 python src_joint/main.py parse \
 --dataset ptb \
 --save-per-sentences 1000 \
@@ -7,5 +19,6 @@ python src_joint/main.py parse \
 --output-path-synconst output_synconst.txt \
 --output-path-syndep output_syndephead.txt \
 --output-path-synlabel output_syndeplabel.txt \
---embedding-path data/glove.gz \
---model-path-base models/joint_bert_dev=95.55_devuas=96.67_devlas=94.86.pt
+--embedding-path data/glove.6B.100d.txt.gz \
+--model-path-base models/joint_cwt_best_dev=93.85_devuas=95.87_devlas=94.47.pt
+# joint_xlnet_dev=96.03_devuas=96.96_devlas=95.32.pt
