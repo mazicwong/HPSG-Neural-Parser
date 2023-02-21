3d9e24452fbe321f857b9340df41d143ed5b06a7
zhiqihuang <zhiqihuang@tencent.com>
2023-02-21 13:03:46 +0800

    .

--- parse_trec_bert.sh
+++ parse_trec_bert.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # trec6 atis trec50 sst5
-export NAME="trec6"
+export NAME="atis"  # trec6
 export TYPE="train valid test"
 export FILE="en.classification.txt text_original.txt"
 for DATA_NAME in ${NAME};
 do
-mkdir parser_output_${DATA_NAME}
+mkdir parser_output_jointbert_${DATA_NAME}
 for DATA_TYPE in ${TYPE};
 do
 for DATA_FILE in ${FILE};
@@ -19,11 +19,11 @@ python src_joint/main.py parse \
 --save-per-sentences 1000 \
 --eval-batch-size 50 \
 --input-path ./tmp.txt \
---output-path-synconst parser_output_${DATA_NAME}/${DATA_TYPE}_${DATA_FILE}_synconst.txt \
---output-path-syndep   parser_output_${DATA_NAME}/${DATA_TYPE}_${DATA_FILE}_syndephead.txt \
---output-path-synlabel parser_output_${DATA_NAME}/${DATA_TYPE}_${DATA_FILE}_syndeplabel.txt \
+--output-path-synconst parser_output_jointbert_${DATA_NAME}/${DATA_TYPE}_${DATA_FILE}_synconst.txt \
+--output-path-syndep   parser_output_jointbert_${DATA_NAME}/${DATA_TYPE}_${DATA_FILE}_syndephead.txt \
+--output-path-synlabel parser_output_jointbert_${DATA_NAME}/${DATA_TYPE}_${DATA_FILE}_syndeplabel.txt \
 --embedding-path data/glove.gz \
---model-path-base models/joint_xlnet_dev=96.03_devuas=96.96_devlas=95.32.pt
+--model-path-base models/joint_bert_dev=95.55_devuas=96.67_devlas=94.86.pt
 
 done
 done
